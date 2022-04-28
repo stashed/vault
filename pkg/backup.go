0f3e982c1f21eb2dcdd4310ab40778feee173f1d
Piyush Kanti Das <piyush123kantidas@gmail.com>
2022-04-28 14:56:44 +0600

    Add support for cross-namespace target. (#607)
    /cherry-pick
    Signed-off-by: piyush1146115 <piyush@appscode.com>

--- pkg/backup.go
+++ pkg/backup.go
@@ -89,6 +89,7 @@ func NewCmdBackup() *cobra.Command {
 				APIVersion: appcatalog.SchemeGroupVersion.String(),
 				Kind:       appcatalog.ResourceKindApp,
 				Name:       opt.appBindingName,
+				Namespace:  opt.appBindingNamespace,
 			}
 			var backupOutput *restic.BackupOutput
 			backupOutput, err = opt.backupMySQL(targetRef)
@@ -123,7 +124,7 @@ func NewCmdBackup() *cobra.Command {
 	cmd.Flags().StringVar(&opt.namespace, "namespace", "default", "Namespace of Backup/Restore Session")
 	cmd.Flags().StringVar(&opt.backupSessionName, "backupsession", opt.backupSessionName, "Name of the Backup Session")
 	cmd.Flags().StringVar(&opt.appBindingName, "appbinding", opt.appBindingName, "Name of the app binding")
-
+	cmd.Flags().StringVar(&opt.appBindingNamespace, "appbinding-namespace", opt.appBindingNamespace, "Namespace of the app binding")
 	cmd.Flags().StringVar(&opt.setupOptions.Provider, "provider", opt.setupOptions.Provider, "Backend provider (i.e. gcs, s3, azure etc)")
 	cmd.Flags().StringVar(&opt.setupOptions.Bucket, "bucket", opt.setupOptions.Bucket, "Name of the cloud bucket/container (keep empty for local backend)")
 	cmd.Flags().StringVar(&opt.setupOptions.Endpoint, "endpoint", opt.setupOptions.Endpoint, "Endpoint for s3/s3 compatible backend or REST backend URL")
@@ -188,7 +189,7 @@ func (opt *mysqlOptions) backupMySQL(targetRef api_v1beta1.TargetRef) (*restic.B
 		return nil, err
 	}
 
-	appBinding, err := opt.catalogClient.AppcatalogV1alpha1().AppBindings(opt.namespace).Get(context.TODO(), opt.appBindingName, metav1.GetOptions{})
+	appBinding, err := opt.catalogClient.AppcatalogV1alpha1().AppBindings(opt.appBindingNamespace).Get(context.TODO(), opt.appBindingName, metav1.GetOptions{})
 	if err != nil {
 		return nil, err
 	}
--- pkg/utils.go
+++ pkg/utils.go
@@ -52,13 +52,14 @@ type mysqlOptions struct {
 	stashClient   stash.Interface
 	catalogClient appcatalog_cs.Interface
 
-	namespace         string
-	backupSessionName string
-	appBindingName    string
-	myArgs            string
-	waitTimeout       int32
-	outputDir         string
-	storageSecret     kmapi.ObjectReference
+	namespace           string
+	backupSessionName   string
+	appBindingName      string
+	appBindingNamespace string
+	myArgs              string
+	waitTimeout         int32
+	outputDir           string
+	storageSecret       kmapi.ObjectReference
 
 	setupOptions  restic.SetupOptions
 	backupOptions restic.BackupOptions
